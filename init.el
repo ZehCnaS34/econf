1539ff1eb23ce863785417d543b6b30ea6c8e7e2
Alexander Sanchez <asanchez.dev@gmail.com>
2017-04-11 00:49:01 -0400

    adding base init

---
+++ init.el
@@ -0,0 +1,91 @@
+;; LETS START HERE
+
+;; Solidify knowledge of emacs
+
+;; making sure emacs loads it's package manager
+;; (THIS IS INCLUDED IN EMACS)
+(require 'package)
+
+
+;; adding repositories for better emacs packages
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+;; activate the emacs packages are are installed in the `elpa' directory
+(package-initialize)
+
+;; on a fresh install, the elpa diretory is not there.
+;; So we need to run the package refresh contents.
+(if (not (file-exists-p "~/.emacs.d/elpa"))
+    (package-refresh-contents))
+
+
+;; Alright, so what we are writing is called `emacs-lisp'.
+;; anyfunction that you see is probably `documented'.
+;; to look at the documentation type : `C-h f'. then type
+;; the function name that you would like to see the docu-
+;; menation for.
+
+;; I want to change the default location of the
+;; auto-generated custome file
+(setq custom-file "~/.emacs.d/custom.el")
+
+;; lets change the locations of the annoying temp files
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+
+;; this package is used for nicer pluggin loading
+(if (not (package-installed-p 'use-package))
+    (package-install 'use-package))
+(require 'use-package)
+
+;; this package is used for better paren closing (LISP stuff)
+(if (not (package-installed-p 'smartparens))
+    (package-install 'smartparens))
+
+;; setting up smart parens
+(require 'smartparens-config)
+(use-package smartparens
+  :config
+  (progn
+    ;; THIS IS WHERE WE ADD THE SMART PAREN FUNCTIONALITY
+    ;; TO THE DIFFERENT LANGUAGES WE WOULD LIKE TO USE IT FOR.
+    (add-hook 'python-mode-hook #'smartparens-mode)
+    (add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
+    ))
+
+
+;; lets now add some color to those parens
+(if (not (package-installed-p 'rainbow-delimiters))
+    (package-install 'rainbow-delimiters))
+(use-package rainbow-delimiters
+  :config
+  (progn
+    (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+    (add-hook 'python-mode-hook 'rainbow-delimiters-mode)
+    (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+    ))
+
+
+;; AWESOME.
+;; it's getting a little annoying having to rewrite that if
+;; statement when I want to install a package...
+;; lets write a little helper function
+(defun ao-install-pkg (pkg-name)
+  (if (not (package-installed-p pkg-name))
+      (package-install pkg-name))) ; cool
+
+
+;; alright, I like using git from emacs... so lets add some magit
+(ao-install-pkg 'magit) ; ah much better with one line... still think
+					; there is a better way tho...
+
+(use-package magit
+  :config
+  (progn
+    (define-key global-map (kbd "M-g s") 'magit-status)
+    ))
